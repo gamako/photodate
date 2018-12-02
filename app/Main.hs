ae4a2c88a0b213cdf1eee3e111d0f310d543ddaa
SUGAMA Tatsuo <gama@sstinc.co.jp>
2018-12-02 11:58:21 +0900

    コード整理

--- app/Main.hs
+++ app/Main.hs
@@ -3,19 +3,19 @@ module Main where
 import Lib
 import qualified Data.Map as Map
 
-meta_data_dirs = [
+metaDataDirs = [
     "../../data/72157675601142498_06440730dcc4_part1",
     "../../data/72157675601142498_06440730dcc4_part2"
     ]
 
-test_dirs = [
-    "../../testdata/part1",
-    "../../testdata/part2"
+testDirs = [
+    "test/testdata/part1",
+    "test/testdata/part2"
     ]
 
 main :: IO ()
 main =  do
-    lis <- readDirectoriesPhotoMap meta_data_dirs
+    lis <- readDirectoriesPhotoMap metaDataDirs
     let a = Map.lookup "10038326853" lis
     print a
 
